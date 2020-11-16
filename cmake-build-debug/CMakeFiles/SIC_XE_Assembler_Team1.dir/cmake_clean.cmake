a0166d7e9799a9550d22e0aa000999fd5b38b979
Leonardo Wang <leo.wangyi57@gmail.com>
2020-11-15 20:51:19 -0800

    initial

---
+++ cmake-build-debug/CMakeFiles/SIC_XE_Assembler_Team1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SIC_XE_Assembler_Team1.dir/main.cpp.o"
+  "SIC_XE_Assembler_Team1"
+  "SIC_XE_Assembler_Team1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SIC_XE_Assembler_Team1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SIC_XE_Assembler_Team1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SIC_XE_Assembler_Team1.
+# This may be replaced when dependencies are built.
